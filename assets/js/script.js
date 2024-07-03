27ec1ebc874c55ae1aacb8eb06623052609b5229
Theo-declerck <declercktheo26@gmail.com>
2024-07-03 11:58:51 +0200

    Push connexion+page acceuil

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+// Menu burger //
 var btn = document.querySelector(".btn");
 var close = document.querySelector("#closebtn");
 var sbm = document.getElementById("sbm");
@@ -10,3 +11,28 @@ function openNav() {
 };
 btn.addEventListener("click", openNav);
 close.addEventListener("click", closeNav);
+// Menu burger //
+
+
+// Apparition de la modal connexion //
+var user = document.querySelector("#user")
+var affi = document.querySelector(".connexion")
+var back = document.querySelector(".back")
+var body = document.querySelector("body")
+var fer = document.querySelector("#closed")
+
+function connexion(){
+    affi.classList.remove("hidden")
+    back.classList.remove("hidden")
+    body.classList.add("scrollx")
+}
+
+function fermer(){
+    affi.classList.add("hidden")
+    back.classList.add("hidden")
+    body.classList.remove("scrollx")
+}
+
+user.addEventListener("click",connexion);
+fer.addEventListener("click",fermer);
+// Apparition de la modal connexion //
